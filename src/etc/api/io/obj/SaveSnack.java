f52a905b9e4a0b157d56e60f929744f2c05689a3
논스톱 <dl7374@naver.com>
2024-03-06 14:17:49 +0900

    # Java io(자바객체 io)

---
+++ src/etc/api/io/obj/SaveSnack.java
@@ -0,0 +1,34 @@
+package etc.api.io.obj;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SaveSnack {
+
+    public static void main(String[] args) {
+
+        List<Snack> snackList = new ArrayList<>();
+        Collections.addAll(snackList,
+                new Snack("콘칲", 1970, 1500, Snack.Taste.GOOD),
+                new Snack("사브레", 1980, 2500, Snack.Taste.BAD),
+                new Snack("오징어집", 1985, 1800, Snack.Taste.SOSO)
+        );
+
+        try(FileOutputStream fos = new FileOutputStream("C:/Myworkspace/snack.sav")){
+
+            // 객체를 저장할 수 있는 보조 스트림
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+            oos.writeObject(snackList);
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
