6915078282e4e42a3dd4c15cc419086f4ebdd34d
논스톱 <dl7374@naver.com>
2024-03-06 17:14:38 +0900

    # Java Basic(음악 관리 프로그램)

--- src/util/Utility.java
+++ src/util/Utility.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Utility {
 
+    public static final String FILE_PATH = "C:/Myworkspace/music";
+
     static Scanner sc;
 
     static {
@@ -12,7 +14,7 @@ public class Utility {
 
     // 입력창에 구분을 위해 줄은 그어주는 메서드
     public static void makeLine(){
-        System.out.println("=============================");
+        System.out.println("=====================================");
     }
 
     // 문자열을 입력받는 기능
